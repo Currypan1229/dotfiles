e943c868e46b58793e4903011532bc01b7c7912a
ROBOT <tkata12@outlook.jp>
2026-03-09 14:39:02 +0900

    fix: cmp

--- nvim/lua/plugins/specs/cmp/nvim-cmp.lua
+++ nvim/lua/plugins/specs/cmp/nvim-cmp.lua
@@ -7,8 +7,8 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/cmp-vsnip",
-        "onsails/lspkind.nvim",
         "folke/noice.nvim",
+        "onsails/lspkind.nvim",
     },
     config = function()
         local cmp = require("cmp")
@@ -16,17 +16,24 @@ return {
 
         cmp.setup({
             fields = { "abbr", "icon", "kind", "menu" },
---            formatting = lspkind.cmp_format({
---                maxwidth = {
---                    menu = 50,
---                    abbr = 50,
---                },
---                ellipsis_char = "...",
---                show_labelDetails = true,
---                before = function (entry, vim_item)
---                    return vim_item
---                end
---            }),
+            format = lspkind.cmp_format({
+                maxwidth = {
+                    -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+                    menu = 50,  -- leading text (labelDetails)
+                    abbr = 50,  -- actual suggestion item
+                },
+                ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+                -- The function below will be called before any actual modifications from lspkind
+                -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                before = function(entry, vim_item)
+                    -- ...
+                    return vim_item
+                end
+            }),
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
