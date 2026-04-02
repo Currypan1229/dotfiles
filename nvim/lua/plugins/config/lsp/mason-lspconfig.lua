2536431cf0b7ce66e2f544e5bdcac4ec416dd330
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-03 00:00:26 +0900

    update: remove unused LSP configurations and clean up mason-lspconfig setup

--- nvim/lua/plugins/config/lsp/mason-lspconfig.lua
+++ nvim/lua/plugins/config/lsp/mason-lspconfig.lua
@@ -1,17 +1,20 @@
 require("mason-lspconfig").setup({
     ensure_installed = {
         "rust_analyzer", -- Rust
-        "jdtls", -- Java
-        "lua_ls", -- Lua
-        "pyright", -- Python
-        "ts_ls", -- TypeScript/JavaScript
-        "html", -- Html
-        "cssls", -- CSS
-        "sqls", -- SQL
+        "jdtls",         -- Java
+        "lua_ls",        -- Lua
+        "pyright",       -- Python
+        "ts_ls",         -- TypeScript/JavaScript
+        -- "eslint", -- TypeScript/JavaScript
+        -- "biome",         -- TypeScript/JavaScript
+        -- "vtsls",  -- TypeScript/JavaScript,
+        "html",   -- Html
+        "cssls",  -- CSS
+        "sqls",   -- SQL
         "bashls", -- Bash
         "jsonls", -- JSON
         "yamlls", -- YAML
-        "taplo" -- TOML
+        "taplo"   -- TOML
     },
     automatic_enable = true
 })
