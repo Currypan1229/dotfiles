5fa8cf73f2e0e377633f0d80e0c1791d1d2eb3b8
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-05 00:01:49 +0900

    update: add fmter and ripgrep

--- nvim/lua/plugins/config/lsp/mason-tool-installer.lua
+++ nvim/lua/plugins/config/lsp/mason-tool-installer.lua
@@ -3,7 +3,15 @@ vim.env.PATH = mason_bin .. ";" .. vim.env.PATH
 
 require("mason-tool-installer").setup({
     ensure_installed = {
-        "lazygit"
+        "stylua",
+        "prettier",
+        "shfmt",
+        "sql-formatter",
+        "marksman",
+        "black",
+
+        "lazygit",
+        "ripgrep"
     },
     auto_install = true,
     run_on_start = true,
