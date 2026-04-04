109f7cfb3123c8a83f48c9834dc854568e6f7960
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-04 14:43:33 +0900

    update: fix colorscheme name in lazy.lua

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
+            { out,                            "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -49,4 +49,4 @@ require("lazy").setup({
     checker = { enabled = true },
 })
 
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("catppuccin-nvim")
