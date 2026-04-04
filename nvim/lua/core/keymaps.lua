bd0886343f4a15067991aa5f80fca4ebc14022c4
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-05 00:00:41 +0900

    update: add the keymap to format files

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -27,3 +27,50 @@ keymap.set("", "<f1>", function()
         prof.start("*")
     end
 end)
+
+keymap.set("", "<leader>lf", function()
+    local buf = vim.api.nvim_get_current_buf()
+    local ft = vim.bo[buf].filetype
+
+    local fmt = require("modules.formatter")
+    if ft == "neo-tree" or ft == "netrw" then
+        local path = nil
+        if ft == "neo-tree" then
+            local state = require("neo-tree.sources.manager").get_state("filesystem")
+            local node = state.tree:get_node()
+
+            if node then
+                path = node:get_id()
+            end
+        elseif ft == "netrw" then
+            local cur_dir = vim.b.netrw_curdir
+            local cur_file = vim.fn.expand("<cfile>")
+
+            if cur_file and cur_file ~= "" then
+                path = vim.fs.joinpath(cur_dir, cur_file)
+            end
+        end
+
+        if path == nil then
+            return Snacks.notify.error("Formatting target was not found.")
+        end
+
+        fmt.format_path(path, function(path, err)
+            if not err then
+                Snacks.notify.info("Formatted: " .. path)
+            else
+                Snacks.notify.error(err)
+            end
+        end)
+    else
+        if vim.bo[buf].modifiable then
+            fmt.format_buf(buf, function(err)
+                if not err then
+                    Snacks.notify.info("Formatted.")
+                else
+                    Snacks.notify.error(err)
+                end
+            end)
+        end
+    end
+end)
---
+++ nvim/lua/modules/formatter.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+function M.format_buf(buf, callback)
+    require("conform").format({
+        bufnr = buf,
+        async = true,
+    }, function(err)
+        if not err then
+            vim.api.nvim_buf_call(buf, function()
+                vim.cmd("silent! write")
+            end)
+        end
+
+        if callback then
+            callback(err)
+        end
+    end)
+end
+
+function M.format_path(path, callback)
+    if vim.fn.isdirectory(path) == 1 then
+        for name, _ in vim.fs.dir(path) do
+            M.format_path(vim.fs.joinpath(path, name), callback)
+        end
+    elseif vim.fn.filereadable(path) == 1 then
+        local buf = vim.fn.bufnr(path)
+        local new_buf = false
+        if buf == -1 then
+            buf = vim.fn.bufadd(path)
+            vim.fn.bufload(buf)
+            new_buf = true
+        end
+        if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modifiable then
+            M.format_buf(buf, function(err)
+                if new_buf then
+                    vim.api.nvim_buf_delete(buf, { force = true })
+                end
+
+                callback(path, err)
+            end)
+        end
+    end
+end
+
+return M
