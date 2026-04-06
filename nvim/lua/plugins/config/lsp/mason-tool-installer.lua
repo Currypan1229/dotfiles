948ddb1681ad7e0cf9579d1f3ba9a53209ffa245
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-07 03:40:05 +0900

    fix: fixed the local registry registration failure because yq could not be found.

--- nvim/lua/plugins/config/lsp/mason-tool-installer.lua
+++ nvim/lua/plugins/config/lsp/mason-tool-installer.lua
@@ -1,58 +1,37 @@
 local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
 vim.env.PATH = mason_bin .. ";" .. vim.env.PATH
 
-local reg = require("mason-registry")
+local registry = require("mason-registry")
 local installer = require("mason-tool-installer")
 
-local function install(libs)
-    for k, v in inpairs(libs) do
-        local pkg = reg.get_package("k")
-        installer.install(pkg, function() 
-            if v ~= nil then
-                v.install
-            end
-        end)
-    end
-end
-
 installer.setup({
-    ensure_installed = {
-        "yq",
-        "lazygit",
-        "ripgrep",
-    },
-    auto_install = true,
+    ensure_installed = {},
     run_on_start = true,
 })
 
-local function contains(t, val)
-    for _, value in ipairs(t) do
-        if value == val then
-            return true
+local function install(apps)
+    local yq_pkg = registry.get_package("yq")
+    yq_pkg:install()
+
+    if not yq_pkg:is_installed() then
+        vim.wait(20000, function()
+            return yq_pkg:is_installed()
+        end, 100)
+    end
+
+    registry.refresh()
+
+    for _, pkg_name in ipairs(apps) do
+        local ok, pkg = pcall(registry.get_package, pkg_name)
+        if ok then
+            pkg:install()
+        else
+            error(pkg_name .. " was not found.")
         end
     end
-    return false
 end
 
-local function resolve_dependency(deps)
-    return resolve_dependency(null: deps, {})
-end
-
----@param deps table
-local function resolve_dependency_internal(deps, cache)
-    let res
-    for k in inpairs(cache.visited) do
-        contains(cache.visited, deps)
-    end
-
-    for _dep, _deps in inpairs(deps) do
-        if type(dep) == "string" then
-        elseif type(dep) == "table" then
-            local res = resolve_dependency_internal(_deps)
-        end
-        local res = resolve_dependency_internal(dep)
-    end
-
-    return true, res
-end
-
+install({
+    "lazygit",
+    "ripgrep",
+})
