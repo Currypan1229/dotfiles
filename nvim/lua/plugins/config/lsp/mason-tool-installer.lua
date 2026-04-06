8d53d5cffa5a8a63c17052c9ac921e9d387df2ac
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-06 12:31:27 +0900

    update: add broken func for mason installer

--- nvim/lua/plugins/config/lsp/mason-tool-installer.lua
+++ nvim/lua/plugins/config/lsp/mason-tool-installer.lua
@@ -1,11 +1,58 @@
 local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
 vim.env.PATH = mason_bin .. ";" .. vim.env.PATH
 
-require("mason-tool-installer").setup({
+local reg = require("mason-registry")
+local installer = require("mason-tool-installer")
+
+local function install(libs)
+    for k, v in inpairs(libs) do
+        local pkg = reg.get_package("k")
+        installer.install(pkg, function() 
+            if v ~= nil then
+                v.install
+            end
+        end)
+    end
+end
+
+installer.setup({
     ensure_installed = {
+        "yq",
         "lazygit",
         "ripgrep",
     },
     auto_install = true,
     run_on_start = true,
 })
+
+local function contains(t, val)
+    for _, value in ipairs(t) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+
+local function resolve_dependency(deps)
+    return resolve_dependency(null: deps, {})
+end
+
+---@param deps table
+local function resolve_dependency_internal(deps, cache)
+    let res
+    for k in inpairs(cache.visited) do
+        contains(cache.visited, deps)
+    end
+
+    for _dep, _deps in inpairs(deps) do
+        if type(dep) == "string" then
+        elseif type(dep) == "table" then
+            local res = resolve_dependency_internal(_deps)
+        end
+        local res = resolve_dependency_internal(dep)
+    end
+
+    return true, res
+end
+
