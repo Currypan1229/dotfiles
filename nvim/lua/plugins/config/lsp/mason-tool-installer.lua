140c15294d6821643888961be02071468951fa46
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-07 14:46:24 +0900

    update: fix mason registry initializer

--- nvim/lua/plugins/config/lsp/mason-tool-installer.lua
+++ nvim/lua/plugins/config/lsp/mason-tool-installer.lua
@@ -1,36 +1,10 @@
-local mason_bin = vim.fn.stdpath("data") .. "/mason/bin"
-vim.env.PATH = mason_bin .. ";" .. vim.env.PATH
-
-local registry = require("mason-registry")
-local installer = require("mason-tool-installer")
-
-installer.setup({
-    ensure_installed = {},
+require("mason-tool-installer").setup({
+    ensure_installed = {
+        "lazygit",
+        "ripgrep",
+    },
+    auto_update = true,
     run_on_start = true,
 })
 
-local function install(apps)
-    local yq_pkg = registry.get_package("yq")
-
-    if not yq_pkg:is_installed() then
-        yq_pkg:install()
-        vim.wait(20000, function()
-            return yq_pkg:is_installed()
-        end, 100)
-        registry.refresh()
-    end
-
-    for _, pkg_name in ipairs(apps) do
-        local ok, pkg = pcall(registry.get_package, pkg_name)
-        if ok then
-            pkg:install()
-        else
-            error(pkg_name .. " was not found.")
-        end
-    end
-end
-
-install({
-    "lazygit",
-    "ripgrep",
-})
+vim.cmd(":MasonToolsInstall")
