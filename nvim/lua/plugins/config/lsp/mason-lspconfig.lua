11d9d50dcf986985fabb1a91c5abca4cc1e8a59f
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-07 15:56:09 +0900

    update: change to sql-language-server

--- nvim/lua/plugins/config/lsp/mason-lspconfig.lua
+++ nvim/lua/plugins/config/lsp/mason-lspconfig.lua
@@ -1,20 +1,20 @@
 require("mason-lspconfig").setup({
     ensure_installed = {
         "rust_analyzer", -- Rust
-        "jdtls",         -- Java
-        "lua_ls",        -- Lua
-        "pyright",       -- Python
-        "ts_ls",         -- TypeScript/JavaScript
+        "jdtls", -- Java
+        "lua_ls", -- Lua
+        "pyright", -- Python
+        "ts_ls", -- TypeScript/JavaScript
         -- "eslint", -- TypeScript/JavaScript
         -- "biome",         -- TypeScript/JavaScript
         -- "vtsls",  -- TypeScript/JavaScript,
-        "html",   -- Html
-        "cssls",  -- CSS
-        "sqls",   -- SQL
+        "html", -- Html
+        "cssls", -- CSS
+        "sql-language-server", -- SQL
         "bashls", -- Bash
         "jsonls", -- JSON
         "yamlls", -- YAML
-        "taplo"   -- TOML
+        "taplo", -- TOML
     },
-    automatic_enable = true
+    automatic_enable = true,
 })
