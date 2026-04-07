50f22dcc03ab0c2227f629aaa371ced980c84b3f
ROBOT <tkata12@outlook.jp>
2026-04-07 21:57:03 +0900

    fix: refactor nvim-cmp mapping to use external keymap configuration

---
+++ nvim/lua/keymaps/plugins/nvim-cmp.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+local cmp = require("cmp")
+
+M.mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+})
+
+return M
