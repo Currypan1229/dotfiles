38e372880a390ca53da7496f501f89cc4b05872d
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-09 01:41:21 +0900

    update: fmt lspconfig

--- nvim/lua/plugins/config/lsp/lspconfig.lua
+++ nvim/lua/plugins/config/lsp/lspconfig.lua
@@ -111,7 +111,7 @@ vim.lsp.config("html", {
     on_attach = function(client, bufnr)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
-    end,
+    end
 })
 vim.lsp.enable("html")
 
