0f4be9228a3189cec2743aa754be33567576f406
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-10 05:32:26 +0900

    update: optimize plugin load events

--- nvim/lua/plugins/specs/cmp/nvim-cmp.lua
+++ nvim/lua/plugins/specs/cmp/nvim-cmp.lua
@@ -1,6 +1,6 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = { "VeryLazy" },
+    event = { "InsertEnter" },
     dependencies = {
         "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
