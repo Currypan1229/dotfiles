75a3167bb6b62ca9f1628e3032c9c7b63562ae2c
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-12 00:03:39 +0900

    refactor: Improve readability by introduce a multiple config loading function

--- nvim/lua/plugins/config/lsp/lspconfig.lua
+++ nvim/lua/plugins/config/lsp/lspconfig.lua
@@ -7,7 +7,7 @@ local LspConfig = {
 LspConfig.__index = LspConfig
 
 ---@param config LspConfig lsp configuration to initialize
-local function loadLsp(config)
+local function load_lsp_config(config)
     local config = setmetatable(config, LspConfig)
     if config.name == nil then
         error("Lsp Name is nil")
@@ -26,20 +26,23 @@ local function loadLsp(config)
     end
 end
 
+local function load_lsp_configs(configs)
+    for _, config in ipairs(configs) do
+        load_lsp_config(config)
+    end
+end
+
 -- JS/TS/JSON/CSS/HTMLのフォーマットのみ
-loadLsp(require("plugins.config.lsp.lspconfig.biome"))
-
--- 型チェックと補完のみ
-loadLsp(require("plugins.config.lsp.lspconfig.vtsls"))
-
--- Biome未対応言語のみに
-loadLsp(require("plugins.config.lsp.lspconfig.eslint"))
-
--- JSONのスキーマ検証(整形はBiome)
-loadLsp(require("plugins.config.lsp.lspconfig.jsonls"))
-
--- htmlタグ補完用(整形はBiome)
-loadLsp(require("plugins.config.lsp.lspconfig.html"))
-
--- CSS補完用(整形はBiome)
-loadLsp(require("plugins.config.lsp.lspconfig.cssls"))
+load_lsp_configs({
+    require("plugins.config.lsp.lspconfig.biome"),
+    -- 型チェックと補完のみ
+    require("plugins.config.lsp.lspconfig.vtsls"),
+    -- Biome未対応言語のみに
+    require("plugins.config.lsp.lspconfig.eslint"),
+    -- JSONのスキーマ検証(整形はBiome)
+    require("plugins.config.lsp.lspconfig.jsonls"),
+    -- htmlタグ補完用(整形はBiome)
+    require("plugins.config.lsp.lspconfig.html"),
+    -- CSS補完用(整形はBiome)
+    require("plugins.config.lsp.lspconfig.cssls"),
+})
