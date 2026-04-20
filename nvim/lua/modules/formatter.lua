6acf98107bfa9393efb9e2393a025ce6a2e0737b
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-21 02:14:47 +0900

    refactor: refactor formatter

--- nvim/lua/modules/formatter.lua
+++ nvim/lua/modules/formatter.lua
@@ -5,34 +5,64 @@ function M.format_buf(buf)
         bufnr = buf,
         async = false,
     })
+    local is_modified = vim.api.nvim_get_option_value("modified", { buf = buf })
+    if is_modified then
+        vim.notify("modified: " .. tostring(is_modified))
+    end
     if vim.bo[buf].modified then
         vim.api.nvim_buf_call(buf, function()
             vim.cmd("silent! write")
         end)
     end
+    vim.wait(200, function()
+        return vim.api.nvim_get_option_value("modified", { buf = buf })
+    end)
+end
+
+local function collect_files(path)
+    local files = {}
+
+    local target_paths = { path }
+    while #target_paths ~= 0 do
+        local new_target_paths = {}
+
+        for _, target_path in ipairs(target_paths) do
+            if vim.fn.isdirectory(target_path) == 1 then
+                for name, _ in vim.fs.dir(target_path) do
+                    table.insert(new_target_paths, vim.fs.joinpath(target_path, name))
+                end
+            else
+                table.insert(files, target_path)
+            end
+        end
+
+        target_paths = new_target_paths
+    end
+
+    return files
 end
 
 function M.format_path(path, callback)
-    if vim.fn.isdirectory(path) == 1 then
-        for name, _ in vim.fs.dir(path) do
-            M.format_path(vim.fs.joinpath(path, name), callback)
-        end
-    elseif vim.fn.filereadable(path) == 1 then
-        local buf = vim.fn.bufnr(path)
-        local new_buf = false
-        if buf == -1 then
-            buf = vim.fn.bufadd(path)
-            vim.fn.bufload(buf)
-            new_buf = true
-        end
-        if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modifiable then
-            if callback then
-                callback(path)
-            end
+    local files = collect_files(path)
 
-            M.format_buf(buf)
-            if new_buf then
-                vim.api.nvim_buf_delete(buf, { force = true })
+    for _, file in ipairs(files) do
+        if vim.fn.filereadable(file) == 1 then
+            local buf = vim.fn.bufnr(file)
+            local new_buf = false
+            if buf == -1 then
+                buf = vim.fn.bufadd(file)
+                vim.fn.bufload(buf)
+                new_buf = true
+            end
+            if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modifiable then
+                if callback then
+                    callback(file)
+                end
+
+                M.format_buf(buf)
+                if new_buf then
+                    vim.api.nvim_buf_delete(buf, { force = true })
+                end
             end
         end
     end
