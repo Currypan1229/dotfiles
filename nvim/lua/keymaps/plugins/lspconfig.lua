6493b665bcea6e06f26f10eae658eef88552f099
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-27 03:22:46 +0900

    update: refactor keymaps and add hover

--- nvim/lua/keymaps/plugins/lspconfig.lua
+++ nvim/lua/keymaps/plugins/lspconfig.lua
@@ -5,6 +5,7 @@ table.insert(keys, {
     vim.lsp.buf.rename,
     desc = "Rename field",
 })
+
 table.insert(keys, {
     "<leader>lsu",
     vim.lsp.buf.references,
@@ -31,4 +32,50 @@ table.insert(keys, {
     desc = "Jump to type definition",
 })
 
+table.insert(keys, {
+    "<leader>lh",
+    vim.lsp.buf.hover,
+    desc = "Hover",
+})
+
+table.insert(keys, {
+    "<leader>lf",
+    function()
+        local buf = vim.api.nvim_get_current_buf()
+        local ft = vim.bo[buf].filetype
+
+        local fmt = require("modules.formatter")
+        if ft == "neo-tree" or ft == "netrw" then
+            local path = nil
+            if ft == "neo-tree" then
+                local state = require("neo-tree.sources.manager").get_state("filesystem")
+                local node = state.tree:get_node()
+
+                if node then
+                    path = node:get_id()
+                end
+            elseif ft == "netrw" then
+                local cur_dir = vim.b.netrw_curdir
+                local cur_file = vim.fn.expand("<cfile>")
+
+                if cur_file and cur_file ~= "" then
+                    path = vim.fs.joinpath(cur_dir, cur_file)
+                end
+            end
+
+            if path == nil then
+                return Snacks.notify.error("Formatting target was not found.")
+            end
+
+            Snacks.notify.info("Formatting.")
+            fmt.format_path(path, nil)
+        else
+            if vim.bo[buf].modifiable then
+                Snacks.notify.info("Formatting.")
+                fmt.format_buf(buf)
+            end
+        end
+    end,
+    desc = "Format files",
+})
 return keys
