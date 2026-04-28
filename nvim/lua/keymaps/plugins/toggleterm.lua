3af29661c1e058dc7089452840cc4d24cab83cc9
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-04-29 07:05:41 +0900

    update: update keymaps for togglemterm

--- nvim/lua/keymaps/plugins/toggleterm.lua
+++ nvim/lua/keymaps/plugins/toggleterm.lua
@@ -1,9 +1,36 @@
 local keys = {}
 
 table.insert(keys, {
-    "<leader>ot",
+    "<leader>ota",
     "<cmd>ToggleTerm<cr>",
     desc = "Toggle Terminal",
+    silent = true,
+})
+table.insert(keys, {
+    "<leader>ott",
+    function()
+        Snacks.picker.buffers({
+            filter = {
+                filter = function(item)
+                    Snacks.notify.notify(vim.bo[item.buf].buftype, {})
+                    return vim.bo[item.buf].buftype == "terminal"
+                end,
+            },
+            nofile = true,
+        })
+    end,
+    desc = "Open New Terminal",
+    silent = true,
+})
+table.insert(keys, {
+    "<leader>otn",
+    function()
+        local term = require("toggleterm.terminal").Terminal
+
+        local term_win = term:new()
+        term_win:toggle()
+    end,
+    desc = "Open New Terminal",
     silent = true,
 })
 
