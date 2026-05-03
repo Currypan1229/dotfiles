418945c6d07ff139c797c73f33d527895ffda28d
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-05-04 00:28:12 +0900

    fix: fix params for create dir

--- scripts/setup/windows/wezterm.ps1
+++ scripts/setup/windows/wezterm.ps1
@@ -1,9 +1,11 @@
 $cwd = (Get-Location).Path
-$item = "~/.config/wezterm"
+$cfg_dir ="~/.config"
+$item = "${cfg_dir}/wezterm"
 
 if (Test-Path -Path $item) {
 	Remove-Item -Path $item
 	Write-Host "Deleted exists symlink."
 }
 
-New-Item -Path $item -Value "${cwd}\wezterm" -ItemType SymbolicLink | Out-Null
+New-Item -Path $cfg_dir -ItemType Directory | Out-Null
+New-Item -Path $item -Value $cwd -ItemType SymbolicLink | Out-Null
