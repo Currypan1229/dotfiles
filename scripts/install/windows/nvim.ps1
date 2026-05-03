0266be5f0a94240529082f75d79fc21bff039e1b
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-05-03 23:31:27 +0900

    update: bypass installer hash for winget

--- scripts/install/windows/nvim.ps1
+++ scripts/install/windows/nvim.ps1
@@ -6,5 +6,15 @@ if ($installed) {
 	Write-Host "Neovim is already installed!" -ForegroundColor Green
 } else {
 	Write-Host "Neovim is not found. Starting installing..." -ForegroundColor Yellow
+
+	$insecured = winget settings export | ConvertFrom-Json
+	if (!($insecured.adminSettings.InstallerHashOverride)) {
+		winget settings --enable InstallerHashOverride
+	}
+
 	winget install --id $pkg_name --exact --force
+
+	if (!($insecured.adminSettings.InstallerHashOverride)) {
+		winget settings --disable InstallerHashOverride
+	}
 }
--- scripts/install/windows/wezterm.ps1
+++ scripts/install/windows/wezterm.ps1
@@ -6,5 +6,15 @@ if ($installed) {
 	Write-Host "Wezterm (Nightly) is already installed!" -ForegroundColor Green
 } else {
 	Write-Host "Wezterm (Nightly) is not found. Starting installing..." -ForegroundColor Yellow
+
+	$insecured = winget settings export | ConvertFrom-Json
+	if (!($insecured.adminSettings.InstallerHashOverride)) {
+		winget settings --enable InstallerHashOverride
+	}
+
 	winget install --id $pkg_name --exact --force
+
+	if (!($insecured.adminSettings.InstallerHashOverride)) {
+		winget settings --disable InstallerHashOverride
+	}
 }
