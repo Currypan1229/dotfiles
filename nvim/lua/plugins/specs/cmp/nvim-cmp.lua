d8e71e1ebbbced33b028a3955e773a620c207dab
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-05-07 00:34:35 +0900

    update: enable cmp when enter command line

--- nvim/lua/plugins/specs/cmp/nvim-cmp.lua
+++ nvim/lua/plugins/specs/cmp/nvim-cmp.lua
@@ -1,6 +1,6 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = { "InsertEnter" },
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
