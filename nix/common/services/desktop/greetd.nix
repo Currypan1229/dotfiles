5c78cc6bf406f8e40c280f5139595c3323650c4f
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-07-05 17:23:51 +0900

    fix: fix xkb exception when launch sway

--- nix/common/services/desktop/greetd.nix
+++ nix/common/services/desktop/greetd.nix
@@ -13,7 +13,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.coreutils}/bin/env XKB_DEFAULT_LAYOUT=jp XKB_DEFAULT_MODEL=jpoadg109a ${pkgs.cage}/bin/cage -s -- ${pkgs.gtkgreet}/bin/gtkgreet -c \"sway -d\"";
+        command = "${pkgs.dbus}/bin/dbus-run-session ${pkgs.coreutils}/bin/env XKB_DEFAULT_LAYOUT=jp XKB_DEFAULT_MODEL=jpoadg109a ${pkgs.cage}/bin/cage -s -- ${pkgs.gtkgreet}/bin/gtkgreet -c \"sway -d\"";
         user = "greeter";
       };
     };
